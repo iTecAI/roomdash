8aac60a8ac926f8b7820f6938d255bcf84a882c3
iTecAI <matteovh@gmail.com>
2021-07-12 16:00:38 -0400

    edits

--- web/script/index.js
+++ web/script/index.js
@@ -17,56 +17,51 @@ function refresh_weather(data) {
     WEATHER = data;
 
     $('.radar').attr('src', '/data/weatherMap?'+Math.random());
-    var dummy_stats = $('<div class="stats"></div>');
+    var dummy_stats = $('<div class="current-weather-stats"></div>');
     dummy_stats.append(
-        $('<span class="weather-basic"></span>')
-            .append($('<img>').attr('src', 'http://openweathermap.org/img/wn/'+WEATHER.current.weather[0].icon+'.png'))
-            .append($('<span></span>').text(WEATHER.current.weather[0].main + ' ('+WEATHER.current.weather[0].description+')'))
+        $('<span class="weather-basic item"></span>')
+            .append($('<img>').attr('src', 'http://openweathermap.org/img/wn/'+WEATHER.current.weather[0].icon+'@2x.png'))
+            .append($('<span class="main"></span>').text(WEATHER.current.weather[0].main))
+            .append($('<span class="desc"></span>').text('[ ' + WEATHER.current.weather[0].description + ' ]'))
     );
     dummy_stats.append(
         $('<span class="temperature item"></span>')
-            .append('<span class="material-icons">device_thermostat</span>')
-            .append(
-                $('<span></span>')
-                    .text(WEATHER.current.temp + ' ' + UNITS[CONFIG.units] + ' (Feels like ' + WEATHER.current.feels_like + ' ' + UNITS[CONFIG.units] + ')')
-            )
-    );
-    dummy_stats.append(
-        $('<span class="humidity item"></span>')
-            .append('<span class="material-icons">water_drop</span>')
-            .append(
-                $('<span></span>')
-                    .text('Humidity: ' + WEATHER.current.humidity + '%')
-            )
-    );
-    dummy_stats.append(
-        $('<span class="wind item"></span>')
-            .append('<span class="material-icons">air</span>')
-            .append(
-                $('<span></span>')
-                    .text('Wind: ' + WEATHER.current.wind_speed + ' ' + SPEED_UNITS[CONFIG.units] + ' @ ' + WEATHER.current.wind_deg + ' deg.')
-            )
-    );
-    dummy_stats.append(
-        $('<span class="uvindex item"></span>')
-            .append('<span class="material-icons">wb_sunny</span>')
-            .append(
-                $('<span></span>')
-                    .text('UV Index: ' + WEATHER.current.uvi)
-            )
+            .append($('<span class="material-icons">thermostat</span>'))
+            .append($('<span class="main"></span>').text(WEATHER.current.temp + ' ' + UNITS[CONFIG.units]))
+            .append($('<span class="desc"></span>').text('[ FEELS LIKE ' + WEATHER.current.feels_like + ' ' + UNITS[CONFIG.units] + ' ]'))
     );
 
-    var sunrise = new Date(WEATHER.current.sunrise*1000);
-    var sunset = new Date(WEATHER.current.sunset*1000);
-    var sun_fmt = 'Sunrise/Sunset: '+sunrise.getHours()+':'+sunrise.getMinutes()+' / '+sunset.getHours()+':'+sunset.getMinutes();
-    
+    if (WEATHER.current.humidity < 30) {
+        var humidityMeaning = '[ LOW HUMIDITY ]';
+    } else if (WEATHER.current.humidity < 55) {
+        var humidityMeaning = '[ OPTIMAL HUMIDITY ]';
+    } else {
+        var humidityMeaning = '[ HIGH HUMIDITY ]';
+    }
     dummy_stats.append(
-        $('<span class="sunriseset item"></span>')
-            .append('<span class="material-icons">brightness_6</span>')
-            .append(
-                $('<span></span>')
-                    .text(sun_fmt)
-            )
+        $('<span class="humidity item"></span>')
+            .append($('<span class="material-icons">water_drop</span>'))
+            .append($('<span class="main"></span>').text(WEATHER.current.humidity + '%'))
+            .append($('<span class="desc"></span>').text(humidityMeaning))
+    );
+
+    var uvi = WEATHER.current.uvi;
+    if (uvi < 3) {
+        var uvim = '[ LOW UVI ]';
+    } else if (uvi < 6) {
+        var uvim = '[ MODERATE UVI ]';
+    } else if (uvi < 8) {
+        var uvim = '[ HIGH UVI ]';
+    } else if (uvi < 11) {
+        var uvim = '[ VERY HIGH UVI ]';
+    } else {
+        var uvim = '[ EXTREME UVI ]';
+    }
+    dummy_stats.append(
+        $('<span class="uvi item"></span>')
+            .append($('<span class="material-icons">wb_sunny</span>'))
+            .append($('<span class="main"></span>').text(WEATHER.current.uvi))
+            .append($('<span class="desc"></span>').text(uvim))
     );
 
     var hourly_weather = $('<div class="weather-hourly"></div>')
@@ -91,9 +86,9 @@ function refresh_weather(data) {
                     .append($('<span></span>').text(tm))
             );
     }
-    dummy_stats.append(hourly_weather);
+    hourly_weather.replaceAll('.weather-hourly')
 
-    dummy_stats.replaceAll('.current-weather-stats .stats');
+    dummy_stats.replaceAll('.current-weather-stats');
 }
 
 function refresh_events(data) {
